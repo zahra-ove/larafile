01b15b33e594d7894deaad61a6fda29d5bd6c1fc
zahra <zahra_ove@yahoo.com>
2020-04-17 17:56:16 +0430

    in middle of star rating system code

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -54,6 +54,26 @@
 
         <!-- Start Left section -->
         <div class="col-md-4">
+
+            <div class="card">
+                <div class="card-header font-weight-bolder">امتیاز به این دوره</div>
+                <div class="card-body">
+                    <div class="container">
+                        <div class="starrating d-flex justify-content-center ">
+                            <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="5 star"></label>
+                            <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="4 star"></label>
+                            <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="3 star"></label>
+                            <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="2 star"></label>
+                            <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="1 star"></label>
+                        </div>
+                  </div>
+                  <div class="container">
+                      <small style="color:red;">میانگین امتیارها:<span> </span> </small>
+                      <small style="color:red">رای<span> </span>از</small>
+                  </div>
+                </div>
+            </div>
+
             <div class="card">
                 <div class="card-header font-weight-bolder">اطلاعات دوره</div>
                 <div class="card-body">
@@ -63,6 +83,7 @@
                     <p>قیمت: <span>{{$file->price}} تومان</span></p>
                 </div>
             </div>
+
         </div>
         <!-- End Left section -->
 
